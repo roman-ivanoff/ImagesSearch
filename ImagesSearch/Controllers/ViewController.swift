c37754c7153493d413eb89380ea84c142e967449
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-11-22 11:37:40 +0200

    added overlay to background image

--- ImagesSearch/Controllers/ViewController.swift
+++ ImagesSearch/Controllers/ViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        addBackgroundOverlay()
+    }
+
+    // MARK: - Custom Methods
+    private func addBackgroundOverlay() {
+        backgroundImage.addoverlay(color: .black, alpha: 0.55)
     }
 }
