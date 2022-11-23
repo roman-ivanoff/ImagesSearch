b338a3b6557261c4de5d72981fb6730c846ce8ee
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-11-23 17:50:44 +0200

    added controllers

---
+++ ImagesSearch/Controllers/ImagesListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImagesListViewController.swift
+//  ImagesSearch
+//
+//  Created by Roman Ivanov on 23.11.2022.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
