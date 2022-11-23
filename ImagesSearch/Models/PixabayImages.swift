771c605757f8e543211d99ca8ead121863803f30
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-11-23 17:48:17 +0200

    added PixabayImages model

---
+++ ImagesSearch/Models/PixabayImages.swift
@@ -0,0 +1,14 @@
+//
+//  PixabayImages.swift
+//  ImagesSearch
+//
+//  Created by Roman Ivanov on 23.11.2022.
+//
+
+import Foundation
+
+struct PixabayImages {
+    let total: Int
+    let totalHits: Int
+    let hits: [ImageHit]
+}
