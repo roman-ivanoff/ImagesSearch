db8041a81fe475112dab1a39a74dd0e9f05e1eb2
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-12-04 14:00:25 +0200

    added cell

---
+++ ImagesSearch/Models/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImagesSearch
+//
+//  Created by Roman Ivanov on 04.12.2022.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
