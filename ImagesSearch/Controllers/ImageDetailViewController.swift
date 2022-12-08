68c47bf341f2cd2afba1431213fe107adb3eba12
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-12-08 17:41:34 +0200

    added views

--- ImagesSearch/Controllers/ImageDetailViewController.swift
+++ ImagesSearch/Controllers/ImageDetailViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ImageDetailViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var relatedCollectionView: UICollectionView!
     @IBOutlet weak var shareButton: ShareButton!
     @IBOutlet weak var downloadButton: SearchButton!
     @IBOutlet weak var apiLicenseLabel: UILabel!
@@ -16,20 +19,27 @@ class ImageDetailViewController: UIViewController {
     @IBOutlet weak var freeLabel: UILabel!
     @IBOutlet weak var imageFormatLabel: UILabel!
     @IBOutlet weak var zoomButton: UIButton!
+
+    // MARK: - Properties
     let imageModel = ImageModel()
     let cellId = "imageCell"
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .red
     }
-    
+
+    // MARK: - Custom Methods
+
+    // MARK: - Actions
     @IBAction func zoomImageAction(_ sender: UIButton) {
     }
     
     @IBAction func downloadImageAction(_ sender: SearchButton) {
     }
+
     @IBAction func shareImageAction(_ sender: ShareButton) {
     }
 }
