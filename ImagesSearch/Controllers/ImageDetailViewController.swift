6952cc9934ca7d55797862ac61c1b02db601c864
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-12-08 17:20:11 +0200

    added views

--- ImagesSearch/Controllers/ImageDetailViewController.swift
+++ ImagesSearch/Controllers/ImageDetailViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ImageDetailViewController: UIViewController {
+    @IBOutlet weak var shareButton: ShareButton!
+    @IBOutlet weak var apiLicenseLabel: UILabel!
+    @IBOutlet weak var noAttributionLabel: UILabel!
     @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var freeLabel: UILabel!
+    @IBOutlet weak var imageFormatLabel: UILabel!
     @IBOutlet weak var zoomButton: UIButton!
     let imageModel = ImageModel()
     let cellId = "imageCell"
@@ -20,5 +25,8 @@ class ImageDetailViewController: UIViewController {
     }
     
     @IBAction func zoomImageAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func shareImageAction(_ sender: ShareButton) {
     }
 }
